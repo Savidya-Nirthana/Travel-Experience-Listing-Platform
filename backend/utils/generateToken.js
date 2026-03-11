013364db60b73d683047857ec41012bbce3cc3f8
Savidya Perera <snirthana5@gmail.com>
2026-03-11 10:22:56 +0530

    cookie issue fixed

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,8 +7,8 @@ const genarateToken = (res, user) => {
   try {
     res.cookie("jwt", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV !== "development",
-      sameSite: "strict",
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
     return token;
