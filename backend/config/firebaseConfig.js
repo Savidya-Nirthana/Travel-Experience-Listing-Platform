eea8b4fe8fb56a190fc3644ee8d974430ed3dda5
Savidya Perera <snirthana5@gmail.com>
2026-03-11 09:51:08 +0530

    firebase security issue fixed

--- backend/config/firebaseConfig.js
+++ backend/config/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC0FFGQXd6Uhpm1N0Y6HkhkZ265c3VWwug",
-  authDomain: "communityconnect-d4e27.firebaseapp.com",
-  projectId: "communityconnect-d4e27",
-  storageBucket: "communityconnect-d4e27.firebasestorage.app",
-  messagingSenderId: "187325142902",
-  appId: "1:187325142902:web:0d347987ff476d71b60c57"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
